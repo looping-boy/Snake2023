0b189e2f1008e357ce2cc7eab03ba7f883ecc6f6
Lotis <guillaume.lotis@gmail.com>
2023-11-20 22:51:48 +0100

    There is 2 different players for 2 laptops now.

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const boxSize = 10;
 
-    // const socket = new WebSocket('ws://52.47.142.196:3000');
-    const socket = new WebSocket('ws://localhost:3000');
-    //socket.onmessage = function(e){ console.log(e.data); };
-    //socket.onopen = () => conn.send('hello');
+    const socket = new WebSocket('ws://52.47.142.196:3000');
+    // const socket = new WebSocket('ws://localhost:3000');
+    socket.onmessage = function(e){ };
+    socket.onopen = () => conn.send('hello');
 
     socket.addEventListener('open', (event) => {
         console.log('WebSocket connection opened');
@@ -20,9 +20,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     socket.addEventListener('message', (event) => {
         const { type, data } = JSON.parse(event.data);
-        console.log("message", event.data)
+        //console.log("message", event.data[0])
         //console.log(JSON.stringify(event.data.lp7eu63))
         if (type === 'snake') {
+            // Iterate over the keys of the data object
+            
+
             drawSnake(data);
         }
     });
@@ -34,16 +37,30 @@ document.addEventListener("DOMContentLoaded", function () {
         socket.send(JSON.stringify({ type: 'keydown', data: { keyCode } }));
     });
 
-    function drawSnake(snake) {
+    function drawSnake(data) {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw the snake
-        for (let i = 0; i < snake.length; i++) {
-            ctx.fillStyle = i === 0 ? "#4CAF50" : "#45a049";
-            ctx.fillRect(snake[i].x, snake[i].y, boxSize, boxSize);
-            ctx.strokeStyle = "#fff";
-            ctx.strokeRect(snake[i].x, snake[i].y, boxSize, boxSize);
+        for (const clientId in data) {
+            //console.log(clientId)
+            if (data.hasOwnProperty(clientId)) {
+                // Get the x and y values for each clientId
+                const xValue = data[clientId][0].x;
+                const yValue = data[clientId][0].y;
+
+                // Log or use the x and y values
+                console.log(`ClientId: ${clientId}, X: ${xValue}, Y: ${yValue}`);
+                // Draw the snake
+                for (let i = 0; i < data[clientId].length; i++) {
+                    ctx.fillStyle = i === 0 ? "#4CAF50" : "#45a049";
+                    ctx.fillRect(data[clientId][i].x, data[clientId][i].y, boxSize, boxSize);
+                    ctx.strokeStyle = "#fff";
+                    ctx.strokeRect(data[clientId][i].x, data[clientId][i].y, boxSize, boxSize);
+                }
+                //drawSnake(data[clientId]);
+            }
         }
+
+        
     }
 });
